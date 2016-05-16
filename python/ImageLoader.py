e3cbf23fc837a6578f1325d903371450131d2328
Caroline Heidenreich <carhei@kth.se>
2016-05-16 12:51:07 +0200

    ImageLoader

--- python/ImageLoader.py
+++ python/ImageLoader.py
@@ -1,10 +1,12 @@
 import numpy as np
+import cv2
+import math
 from PIL import Image
 import matplotlib.pyplot as plt
 
 paths = []
 ellipses = []
-with open('annotations.txt') as inputfile:
+with open('annotations_short.txt') as inputfile:
     prevLine = 0
     getNextLine = 0
     for line in inputfile:
@@ -17,19 +19,37 @@ with open('annotations.txt') as inputfile:
             getNextLine = 0
         prevLine = line
 
-print(paths)
-print(ellipses)
+#print(paths)
+#print(ellipses)
 
 
 imdb = []
 for path in paths:
-    pil_im = Image.open(path + '.jpg').convert('L')
+    pil_im = Image.open('images/' + path + '.jpg').convert('L')
     img = np.array(pil_im)
     img = img - np.mean(img)
     img = img/np.std(img)
     imdb.append(img)
 
 
-for im in imdb:
-    plt.imshow(im,cmap=plt.cm.gray)
-    plt.show()
+#for im in imdb:
+    #plt.imshow(im,cmap=plt.cm.gray)
+    #plt.show()
+
+rectangles = []
+
+for ellipse in ellipses:
+	ellipse = ellipse.split(' ')
+	rmax = float(ellipse[0])
+	rmin = float(ellipse[1])
+	angle = float(ellipse[2])
+	x_center = float(ellipse[3])
+	y_center = float(ellipse[4])
+	width = rmax*math.sin(angle)
+	rectangles.append([x_center, y_center, width])
+	#cv2.rectangles()
+
+print(rectangles)
+
+
+
