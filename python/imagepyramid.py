dd89a71bbf7225d4abac6312afb6b5f3dd864bb9
jolilj <lilja.joakim@gmail.com>
2016-05-16 12:44:04 +0200

    created image wrapper holding image and label and fixed the pyramid representation to handle labels as well

--- python/imagepyramid.py
+++ python/imagepyramid.py
@@ -3,23 +3,51 @@ from PIL import Image
 import cv2
 import imutils
 
+
+## Pyramid representation of an image
+#  labels are stored as [[cx,cy,w], [cx,cy,w]....]
+
 class ImagePyramid:
     # Init with image
-    def __init__(self, image, scale=1.5,  minSize=(32,32)):
-        self.pyramid = self.constructPyramid(image,scale, minSize)
+    def __init__(self, image, label,  scale=1.5,  minSize=(32,32)):
+        self.constructPyramid(image, label, scale, minSize)
         self.scale = scale
+
     # Construct the image pyramid
-    def constructPyramid(self, image, scale, minSize):
+    def constructPyramid(self, image, label,  scale, minSize):
         self.pyramid = []
-        self.pyramid.append(image)
-        im = image
+        im = LabeledImage(image, label)
+        self.pyramid.append(im)
+        prevLabel = label
         while True:
-            w = int(im.shape[1] / scale)
-            im = imutils.resize(image, width=w)
-            print(im.shape)
+            w = int(im.image.shape[1] / scale)
+            label = np.round(prevLabel / scale)
+
+            i = imutils.resize(im.image, width=w)
+            im = LabeledImage(i, label)
+
             self.pyramid.append(im)
-            if (im.shape[0] < minSize[1] or im.shape[1] < minSize[0]):
+            prevLabel = label
+            print(im.image.shape)
+            print(im.label)
+            if (im.image.shape[0] < minSize[1] or im.image.shape[1] < minSize[0]):
                 break
+    
+    # Returns the label expressed as top left corner and bottom right corner for display in matplotlib
+    # [[topx, topy], [bottomx, bottomy]]
+    def labelToRect(self):
+        rects = []
+        for i in self.pyramid:
+            rects.append(i.labelToRect())
+        return rects
 
-            
 
+
+class LabeledImage:
+    def __init__(self,image, label):
+        self.image = image
+        self.label = label
+
+    def labelToRect(self):
+        print(self.label)
+        return [(int(self.label[0]-self.label[2]/2),int(self.label[1]-self.label[2]/2)),(int(self.label[0]+self.label[2]/2), int(self.label[1]+self.label[2]/2))]
--- python/imagetest.py
+++ python/imagetest.py
@@ -2,10 +2,18 @@ import cv2
 import imutils
 import matplotlib.pyplot as plt
 from imagepyramid import ImagePyramid
+import numpy as np
 
 image = cv2.imread('test_im.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-pyr  = ImagePyramid(gray)
+label = np.asarray([int(gray.shape[1]/2), int(gray.shape[0]/2), 150])
+pyr  = ImagePyramid(gray,label)
 
-
+for i in pyr.pyramid:
+    rect = i.labelToRect()
+    print(rect[0])
+    print(rect[1])
+    cv2.rectangle(i.image, rect[0] , rect[1], [0,255,0], 1)
+    plt.imshow(i.image, cmap = 'gray', interpolation = 'bicubic')
+    plt.show()
 
