6213a071f0a53241e63eccff3c4b0c64c563f46c
Caroline Heidenreich <carhei@kth.se>
2016-05-17 13:08:21 +0200

    partwise fixed sublabels

--- python/SlidingWindow.py
+++ python/SlidingWindow.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 #defines a sliding window that is sliding over the image cascade
 
 def slideWindow(imagePyramid, stepSize, windowLength):
+	# labels are stored as [[cx,cy,w], [cx,cy,w]....]
 
 	subwindows = []
 	windowCenters = []
@@ -26,6 +27,18 @@ def slideWindow(imagePyramid, stepSize, windowLength):
 				subwindow = [image[x:x+windowLength], image[y:y+windowLength]]
 				windowCenters.append(windowCenter)
 				subwindows.append(subwindow)
+				labelwidth = label[2]
+				xlabel_left = label[0]-int(labelwidth/2)
+				xlabel_right = label[0]+int(labelwidth/2)
+				ylabel_upper = label[1]+int(labelwidth/2)
+				ylabel_lower = label[1]-int(labelwidth/2)
+				margin = 2.0/labelwidth
+				sublabelx = 1- margin*(math.fabs(x-xlabel_left)+ math.fabs(x+windowLength-xlabel_right))
+				sublabelx = max(sublabelx, 0)
+				sublabely = 1- margin*(math.fabs(y-ylabel_upper)+ math.fabs(y+windowLength-ylabel_lower))
+				sublabely = max(sublabely, 0)
+				print(margin)
+				sublabels[i].append([sublabelx, sublabely])
 				#copy = image.copy()
 				#cv2.rectangle(copy, (x,y), (x+windowLength, y+windowLength), [255, 255, 255],1 )
 				#cv2.imshow("Window", copy)
@@ -47,4 +60,4 @@ rect = imagePyramid.pyramid[0].labelToRect()
 cv2.rectangle(imagePyramid.pyramid[0].image, rect[0], rect[1], [0, 255, 0],1 )
 [windowCenters, sublabels, subwindow] = slideWindow(imagePyramid, 12, 128)
 
-
+print(sublabels)
