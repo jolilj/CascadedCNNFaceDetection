60b9df99b1f657a845ff81f6f6e83e00fa6ddff9
jolilj <lilja.joakim@gmail.com>
2016-05-17 10:41:44 +0200

    created preprocessing function handle

--- python/ImageLoader.py
+++ python/ImageLoader.py
@@ -5,57 +5,49 @@ from imagepyramid import ImagePyramid
 from PIL import Image
 import matplotlib.pyplot as plt
 
-paths = []
-ellipses = []
-with open('annotations_short.txt') as inputfile:
-    prevLine = 0
-    getNextLine = 0
-    for line in inputfile:
-        line = line.strip()
-        if(len(line)==1 and int(line)==1):
-            paths.append(prevLine)
-            getNextLine = 1
-        elif(getNextLine):
-            ellipses.append(line)
-            getNextLine = 0
-        prevLine = line
 
-#print(paths)
-#print(ellipses)
+def loadAndPreProcessIms(annotationsFile, pyramidScale, pyramidMinSize):
+    paths = []
+    ellipses = []
+    imdb = []
+    tempIm = 0 #Init temp image variable
+    with open(annotationsFile) as inputfile:
+        prevLine = 0
+        getNextLine = 0
+        for line in inputfile:
+            line = line.strip()
+            if(len(line)==1 and int(line)==1):
+                #Load image, normalize and make zero mean
+                tempIm = loadAndNormalize(prevLine)
+                getNextLine = 1
+            elif(getNextLine):
+                label = getLabel(line)
+                imdb.append(ImagePyramid(tempIm,label, pyramidScale, pyramidMinSize))
+                getNextLine = 0
+            prevLine = line
+    return imdb
 
-
-imdb = []
-for path in paths:
+def loadAndNormalize(path):
     pil_im = Image.open('images/' + path + '.jpg').convert('L')
     img = np.array(pil_im)
     img = img - np.mean(img)
     img = img/np.std(img)
-    imdb.append(img)
+    return img
 
-
-#for im in imdb:
-    #plt.imshow(im,cmap=plt.cm.gray)
-    #plt.show()
-
-rectangles = []
-ellipsedata = []
-
-for ellipse in ellipses:
-	ellipse = ellipse.split(' ')
+def getLabel(line):
+	ellipse = line.split(' ')
 	rmax = float(ellipse[0])
 	rmin = float(ellipse[1])
 	angle = float(ellipse[2])
 	x_center = float(ellipse[3])
 	y_center = float(ellipse[4])
 	width = int(2*rmax*math.cos(angle*math.pi/180))
-	ellipsedata.append([(int(x_center), int(y_center)), [int(angle)], (int(rmax), int(rmin))])
-	rectangles.append([x_center, y_center, math.fabs(width)])
-	#cv2.rectangles()
-
-print(rectangles)
+	#ellipsedata.append([(int(x_center), int(y_center)), [int(angle)], (int(rmax), int(rmin))])
+	return np.asarray([x_center, y_center, math.fabs(width)])
 
 
-pyr = ImagePyramid(imdb[1], np.asarray(rectangles[1]))
+imdb = loadAndPreProcessIms('annotations.txt', 1.5, (32,32))
+pyr = imdb[0]
 
 rect = pyr.pyramid[1].labelToRect()
 cv2.rectangle(pyr.pyramid[1].image, rect[0], rect[1], [0, 255, 0],1 )
@@ -64,10 +56,5 @@ cv2.rectangle(pyr.pyramid[1].image, rect[0], rect[1], [0, 255, 0],1 )
 #plt.show()
 
 print("===================")
-print(ellipsedata[1][0])
-print(ellipsedata[1][1][0])
-print(ellipsedata[1][2])
-cv2.ellipse(pyr.pyramid[1].image, ellipsedata[1][0], ellipsedata[1][2], 90+ellipsedata[1][1][0], 0, 360, [0, 255,0])
-
 plt.imshow(pyr.pyramid[1].image,cmap=plt.cm.gray)
 plt.show()
